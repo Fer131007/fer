000f48d4380bbbbe2c3886a4b2ca2b2074427cbf
Fer131007 <103433870+Fer131007@users.noreply.github.com>
2024-03-13 08:42:17 -0300

    Main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+const botoes= docment; queryselector All ("botao");
+for i= 0; i<botoes.length;i(++){
+     botoes.onclick= function(){
+
+       for(let j= 0; j<botoes.lenght; j++{
+         botoes.closslist.remove("ativo");
+       }
+       botoes[i].classlist. add("ativo");
+     }
+  botoes[i].classlist. add("ativo");
+}
+}
